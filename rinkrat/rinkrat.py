ab8f0f564065bc2b028ff5ae2507127716de5d13
Alex Reynolds <hellport@protonmail.com>
2024-01-04 14:59:06 -0400

    allow the user to specify a date

--- rinkrat/rinkrat.py
+++ rinkrat/rinkrat.py
@@ -4,12 +4,19 @@ from datetime import datetime
 import api
 import order
 
-def display(result: dict) -> None:
+result = dict()
+
+date = datetime.now()
+today = '{:04d}-{:02d}-{:02d}'.format(date.year, date.month, date.day)
+
+def display(result: dict, date) -> None:
+    print(f'{"Date": <26}{date}')
+
     for group in result:
 
         # group header
         caps = [word.capitalize() for word in group.replace("_", " ").split(" ")]
-        print(' '.join(caps))
+        print(f'{" ".join(caps)}')
 
         print(f'{"Team": <26}{"GP": <4}{"W": <4}{"L": <4}{"OTL": <4}{"PTS": <4}')
 
@@ -21,35 +28,40 @@ def display(result: dict) -> None:
 
         print()
 
-def get_standings(args) -> None:
+def overall(teams, date):
+    result = order.order_by_overall(teams)
+    display(result, date)
 
-    date = datetime.now()
+def conference(teams, date):
+    result = order.order_by_conference(teams)
+    display(result, date)
 
-    now = '{:04d}-{:02d}-{:02d}'.format(date.year, date.month, date.day)
+def division(teams, date):
+    result = order.order_by_division(teams)
+    display(result, date)
 
-    response = api.request_league_standings(now)
+def wild_card(teams, date):
+    result = order.order_by_wild_card(teams)
+    display(result, date)
 
-    teams = response['standings']
-    result = dict()
+class StandingsAction(argparse.Action):
+    def __init__(self, option_strings, order,
+                 *args, **kwargs):
+        self._order = order
+        super(StandingsAction, self).__init__(option_strings=option_strings,
+                                              *args, **kwargs)
 
-    if args.overall:
-        result = order.order_by_overall(teams)
+    def __call__(self, parser, args, values, option_string=None):
+        setattr(args, self.dest, values)
 
-    if args.conference:
-        result = order.order_by_conference(teams)
+        # validate
 
-    if args.division:
-        result = order.order_by_division(teams)
+        response = api.request_league_standings(values)
 
-    if args.wild_card:
-        result = order.order_by_wild_card(teams)
-
-    display(result)
+        self._order(response['standings'], values)
 
 def _parse_args():
     parser = argparse.ArgumentParser(prog = 'rinkrat')
-
-    parser.set_defaults(func = lambda x: None)
 
     subparsers = parser.add_subparsers()
 
@@ -57,21 +69,22 @@ def _parse_args():
 
     standings_group = standings_parser.add_mutually_exclusive_group()
 
-    # accept nothing or a string date (0 or 1 args)
-    standings_group.add_argument('-o', '--overall', action = 'store_true')
+    standings_group.add_argument('-o', '--overall', nargs='?', const=today,
+                                 action=StandingsAction, order=overall)
 
-    standings_group.add_argument('-c', '--conference', action = 'store_true')
-    standings_group.add_argument('-d', '--division', action = 'store_true')
-    standings_group.add_argument('-w', '--wild-card', action = 'store_true')
+    standings_group.add_argument('-c', '--conference', nargs='?', const=today,
+                                 action=StandingsAction, order=conference)
 
-    standings_parser.set_defaults(func = get_standings)
+    standings_group.add_argument('-d', '--division', nargs='?', const=today,
+                                 action=StandingsAction, order=division)
+
+    standings_group.add_argument('-w', '--wild-card', nargs='?', const=today,
+                                 action=StandingsAction, order=wild_card)
 
     return parser.parse_args()
 
 def main() -> None:
     args = _parse_args()
-
-    args.func(args)
 
 if __name__ == '__main__':
     main()
