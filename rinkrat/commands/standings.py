e8948dc740aff10c72a52305da32ad9b6a18f028
Alex Reynolds <hellport@protonmail.com>
2024-01-14 14:15:34 -0400

    new crafty way to rank the raw data

--- rinkrat/commands/standings.py
+++ rinkrat/commands/standings.py
@@ -47,15 +47,13 @@ class Standings:
 
         try:
             getattr(self, args[0].ranking)(args[1])
-
-            print(self.opts)
-
         except AttributeError:
             print(f"\"{args[0].ranking}\" is not a valid ranking", end="\n\n")
             parser.print_help()
 
-        # self.get_data()
-        # self.order_data()
+        self.get_data()
+
+        self.rank_results()
         # self.display()
 
     def get_data(self):
@@ -70,36 +68,6 @@ class Standings:
 
         # store the response data in our opts dict
         self.opts.setdefault("data", api.get(url))
-
-    # TODO - refactor with lastest changes to arg parsing
-    def order_data(self):
-        if "overall" in self.opts:
-            # TODO: refactor - this is duplicating data in a slightly different order
-            self.opts.setdefault(
-                "ranked", 
-                order_by_overall(self.opts["data"]["standings"])
-            )
-
-        if "conference" in self.opts:
-            # TODO: refactor - this is duplicating data in a slightly different order
-            self.opts.setdefault(
-                "ranked", 
-                order_by_conference(self.opts["data"]["standings"])
-            )
-
-        if "division" in self.opts:
-            # TODO: refactor - this is duplicating data in a slightly different order
-            self.opts.setdefault(
-                "ranked", 
-                order_by_division(self.opts["data"]["standings"])
-            )
-
-        if "wild_card" in self.opts:
-            # TODO: refactor - this is duplicating data in a slightly different order
-            self.opts.setdefault(
-                "ranked", 
-                order_by_wild_card(self.opts["data"]["standings"])
-            )
 
     def display(self):
         print(f'{"Date": <26}{self.opts["date"]}')
@@ -121,19 +89,24 @@ class Standings:
             print()
 
     def overall(self, argv):
-        print("overall standings here: {}".format(argv))
+        if argv:
+            argparse.ArgumentParser.error(
+                self=self.parser,
+                message="overall does have a selection, see standings --help")
+        else:
+            self.opts.setdefault("selection", ["overall"])
         
     def conference(self, argv: List[str]):
-        conferences = ["east", "west"]
+        conferences = ["eastern", "western"]
         if any(i not in conferences for i in argv):
             argparse.ArgumentParser.error(
                 self=self.parser,
-                message="you can only use 'east' or 'west', see standings --help")
+                message="you can only use 'eastern' or 'western', see standings --help")
         else:
             selection = []
 
             if not argv:
-                selection = ["east", "west"]
+                selection = ["eastern", "western"]
             else:
                 [selection.append(x) for x in argv if x not in selection]
 
@@ -171,6 +144,49 @@ class Standings:
 
             self.opts.setdefault("selection", selection)
 
+    def rank_results(self):
+        if self.opts["ranking"] == "overall":
+            self.opts.setdefault("ranked", {})
+
+        if self.opts["ranking"] == "conference":
+            self.opts.setdefault(
+                "ranked",
+                ranked_by_conference(self, 
+                                     self.opts["data"]["standings"]))
+
+        if self.opts["ranking"] == "division":
+            self.opts.setdefault(
+                "ranked",
+                ranked_by_division(self, 
+                                   self.opts["data"]["standings"]))
+
+        if self.opts["ranking"] == "wild":
+            self.opts.setdefault("ranked", {})
+
+def ranked_by_conference(self, teams: List) -> dict:
+    result = dict()
+
+    for x in self.opts["selection"]:
+        result.setdefault(x.capitalize(), [])
+
+    for team in teams:
+        if team["conferenceName"] in result:
+            result[team["conferenceName"]].append(team)
+
+    return result
+
+def ranked_by_division(self, teams: List) -> dict:
+    result = dict()
+
+    for x in self.opts["selection"]:
+        result.setdefault(x.capitalize(), [])
+
+    for team in teams:
+        if team["divisionName"] in result:
+            result[team["divisionName"]].append(team)
+
+    return result
+
 def validate_query_date(text: str, parser: argparse.ArgumentParser) -> datetime:
     for format in ("%Y-%m-%d", "%Y.%m.%d", "%Y/%m/%d", "%Y%m%d"):
         try:
@@ -178,100 +194,7 @@ def validate_query_date(text: str, parser: argparse.ArgumentParser) -> datetime:
         except ValueError:
             pass
         
-    argparse.ArgumentParser.error(self=parser, message="invalid date format - use YYYY-MM-DD. see standings --help")
+    argparse.ArgumentParser.error(
+        self=parser, 
+        message="invalid date format - use YYYY-MM-DD. see standings --help")
 
-def order_by_overall(teams: List) -> dict:
-    overall = []
-
-    for team in teams:
-        overall.append(team)
-
-    result = dict()
-    result['league'] = overall
-
-    return result
-
-def order_by_conference(teams: List) -> dict:
-    eastern = []
-    western = []
-
-    for team in teams:
-        
-        if team['conferenceName'] == 'Eastern':
-            eastern.append(team)
-
-        elif team['conferenceName'] == 'Western':
-            western.append(team)
-
-    result = dict()
-    result['eastern'] = eastern
-    result['western'] = western
-
-    return result
-
-def order_by_division(teams: List) -> dict:
-    atlantic = []
-    metropolitan = []
-    central = []
-    pacific = []
-
-    for team in teams:
-
-        if team['divisionName'] == 'Atlantic':
-            atlantic.append(team)
-
-        elif team['divisionName'] == 'Metropolitan':
-            metropolitan.append(team)
-
-        elif team['divisionName'] == 'Central':
-            central.append(team)
-
-        elif team['divisionName'] == 'Pacific':
-            pacific.append(team)
-
-    result = dict()
-    result['atlantic'] = atlantic
-    result['metropolitan'] = metropolitan
-    result['central'] = central
-    result['pacific'] = pacific
-
-    return result
-
-def order_by_wild_card(teams: List) -> dict:
-    atlantic = []
-    metropolitan = []
-    central = []
-    pacific = []
-    wild_card_east = []
-    wild_card_west = []
-
-    for team in teams:
-
-        if team['divisionName'] == 'Atlantic' and team['wildcardSequence'] == 0:
-            atlantic.append(team)
-
-        elif team['divisionName'] == 'Metropolitan' and team['wildcardSequence'] == 0:
-            metropolitan.append(team)
-        
-        elif team['divisionName'] == 'Central' and team['wildcardSequence'] == 0:
-            central.append(team)
-        
-        elif team['divisionName'] == 'Pacific' and team['wildcardSequence'] == 0:
-            pacific.append(team)
-        
-        elif team['conferenceName'] == 'Eastern':
-            wild_card_east.append(team)
-        
-        elif team['conferenceName'] == 'Western':
-            wild_card_west.append(team)
-
-    result = dict()
-
-    result['atlantic'] = atlantic
-    result['metropolitan'] = metropolitan
-    result['wild_card_east'] = wild_card_east
-    result['central'] = central
-    result['pacific'] = pacific
-    result['wild_card_west'] = wild_card_west
-
-    return result
