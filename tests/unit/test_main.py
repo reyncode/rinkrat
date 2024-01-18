564c3f94d47cf8d829a586ec3d1b875a8c4738c8
Alex Reynolds <hellport@protonmail.com>
2024-01-18 10:47:58 -0400

    break up test folder to unit and integration

---
+++ tests/unit/test_main.py
@@ -0,0 +1,15 @@
+import pytest
+
+from rinkrat import main
+
+'''
+    main.py unit test
+
+    - the command passed is a registered attribute of the Cli class
+    - passing an invalid command raises an exception and ends the program
+    - passing a valid command invokes the constructor of the relevant command
+
+'''
+
+def test_cli_has_standings():
+    pass
