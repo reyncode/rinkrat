72f80774c2f80df864b7dd81719ecf5602895343
Alex Reynolds <hellport@protonmail.com>
2024-01-27 18:03:04 -0400

    purge unused files/folders

--- tests/integration/test_integration.py
+++
@@ -1,8 +0,0 @@
-from rinkrat import cli, standings
-
-def test_cli_command_has_constructor():
-    """
-    ensure the appropriate constructor is assigned to each registered
-    cli command attribute.
-    """
-    pass
