4fd02a1008a546fed4b2014168861f4abd052d19
Alex Reynolds <hellport@protonmail.com>
2024-01-29 12:44:10 -0400

    convert data.py into a json file to parse from fixture

--- tests/test_standings.py
+++ tests/test_standings.py
@@ -1,31 +1,54 @@
+import json
+import pathlib
 import pytest
 
 from rinkrat import standings
-from .data import data
+
+TESTS_DIR = pathlib.Path(__file__).resolve().parent
 
 @pytest.fixture
-def _standings() -> standings.Standings:
+def standings_object() -> standings.Standings:
     return standings.Standings()
 
-def test_standings_has_ranking_attrs(_standings):
+@pytest.fixture
+def input_json():
+    with open(TESTS_DIR / "data" / "standings_2023_12_31.json") as file:
+        return json.loads(file.read())
+
+@pytest.fixture
+def expected_json(request):
+    marker = request.node.get_closest_marker("expected_data")
+
+    with open(TESTS_DIR / "data" / "expected_2023_12_31.json") as file:
+        data = json.loads(file.read())
+
+    match len(marker.args):
+        case 1:
+            return data.get(marker.args[0], {})
+        case 2:
+            return data.get(marker.args[0], {}).get(marker.args[1], {})
+        case _:
+            return data
+
+def test_standings_has_ranking_attrs(standings_object):
     """
     does the object have callable attributes set for each ranking
     """
     attrs = ["overall", "conference", "division", "wild"]
 
     for attr in attrs:
-        assert hasattr(_standings, attr)
+        assert hasattr(standings_object, attr)
 
-def test_standings_exit_on_bad_ranking(_standings):
+def test_standings_exit_on_bad_ranking(standings_object):
     """
     does the program raise a SystemExit when a bad ranking is passed
     """
     args = ["invalid"]
 
     with pytest.raises(SystemExit):
-        _standings.parse(args)
+        standings_object.parse(args)
 
-def test_standings_ranking_exit_on_bad_args(_standings):
+def test_standings_ranking_exit_on_bad_args(standings_object):
     """
     does the program raise a SystemExit when an invalid argument is passed
     to a ranking
@@ -42,10 +65,10 @@ def test_standings_ranking_exit_on_bad_args(_standings):
             argv = [k]
             [argv.append(a) for a in v]
 
-            _standings.parse(argv)
-            _standings.execute()
+            standings_object.parse(argv)
+            standings_object.execute()
 
-def test_standings_ranking_default_selection(_standings):
+def test_standings_ranking_default_selection(standings_object):
     """
     does the object contain the expected selections when defaults are used
     for the ranking
@@ -58,9 +81,9 @@ def test_standings_ranking_default_selection(_standings):
     }
 
     for ranking in defaults.keys():
-        _standings.parse([ranking])
-        _standings.execute()
-        assert defaults[ranking] == _standings.opts["selection"]
+        standings_object.parse([ranking])
+        standings_object.execute()
+        assert defaults[ranking] == standings_object.opts["selection"]
 
 def test_standings_is_valid_query_date():
     """
@@ -70,70 +93,103 @@ def test_standings_is_valid_query_date():
     assert standings._is_valid_query_date("2010-12-31") == True
     assert standings._is_valid_query_date("12.345/6789") == False
 
-def test_standings_exit_on_bad_date(_standings):
-    """
-    does the program exit with a SystemExit if a bad date is passed to -d
-    """
+def test_standings_exit_on_bad_date(standings_object):
     args = ["overall", "-d", "785-8993.abcd,*&^%"]
 
     with pytest.raises(SystemExit):
-        _standings.parse(args)
-        _standings.execute()
+        standings_object.parse(args)
+        standings_object.execute()
 
-def test_standings_ranking_overall():
-    result = standings._ranked_overall(data.teams_2023_12_31)
+@pytest.mark.expected_data("overall", "overall")
+def test_standings_ranking_overall(input_json, expected_json):
+    result = standings._ranked_overall(input_json["standings"])
 
-    for i, team in enumerate(result["Overall"]):
-        assert data.overall_2023_12_31[i] == team.get('teamName').get('default')
+    for i, input_team in enumerate(result["Overall"]):
+        expected_team_name = expected_json[str(i)]
+        input_team_name = input_team.get('teamName').get('default')
+
+        assert expected_team_name == input_team_name
         
-def test_standings_ranking_eastern_conference():
-    result = standings._ranked_conference(["eastern"], data.teams_2023_12_31)
+@pytest.mark.expected_data("conference", "eastern")
+def test_standings_ranking_eastern_conference(input_json, expected_json):
+    result = standings._ranked_conference(["eastern"], input_json["standings"])
 
-    for i, team in enumerate(result["Eastern"]):
-        assert data.eastern_conference_2023_12_31[i] == team.get('teamName').get('default')
+    for i, input_team in enumerate(result["Eastern"]):
+        expected_team_name = expected_json[str(i)]
+        input_team_name = input_team.get('teamName').get('default')
 
-def test_standings_ranking_western_conference():
-    result = standings._ranked_conference(["western"], data.teams_2023_12_31)
+        assert expected_team_name == input_team_name
 
-    for i, team in enumerate(result["Western"]):
-        assert data.western_conference_2023_12_31[i] == team.get('teamName').get('default')
+@pytest.mark.expected_data("conference", "western")
+def test_standings_ranking_western_conference(input_json, expected_json):
+    result = standings._ranked_conference(["western"], input_json["standings"])
 
-def test_standings_ranking_atlantic_division():
-    result = standings._ranked_division(["atlantic"], data.teams_2023_12_31)
+    for i, input_team in enumerate(result["Western"]):
+        expected_team_name = expected_json[str(i)]
+        input_team_name = input_team.get('teamName').get('default')
 
-    for i, team in enumerate(result["Atlantic"]):
-        assert data.atlantic_division_2023_12_31[i] == team.get('teamName').get('default')
+        assert expected_team_name == input_team_name
+
+@pytest.mark.expected_data("division", "atlantic")
+def test_standings_ranking_atlantic_division(input_json, expected_json):
+    result = standings._ranked_division(["atlantic"], input_json["standings"])
+
+    for i, input_team in enumerate(result["Atlantic"]):
+        expected_team_name = expected_json[str(i)]
+        input_team_name = input_team.get('teamName').get('default')
+
+        assert expected_team_name == input_team_name
     
-def test_standings_ranking_metropolitan_division():
-    result = standings._ranked_division(["metropolitan"], data.teams_2023_12_31)
+@pytest.mark.expected_data("division", "metropolitan")
+def test_standings_ranking_metropolitan_division(input_json, expected_json):
+    result = standings._ranked_division(["metropolitan"], input_json["standings"])
 
-    for i, team in enumerate(result["Metropolitan"]):
-        assert data.metropolitan_division_2023_12_31[i] == team.get('teamName').get('default')
+    for i, input_team in enumerate(result["Metropolitan"]):
+        expected_team_name = expected_json[str(i)]
+        input_team_name = input_team.get('teamName').get('default')
 
-def test_standings_ranking_central_division():
-    result = standings._ranked_division(["central"], data.teams_2023_12_31)
+        assert expected_team_name == input_team_name
 
-    for i, team in enumerate(result["Central"]):
-        assert data.central_division_2023_12_31[i] == team.get('teamName').get('default')
+@pytest.mark.expected_data("division", "central")
+def test_standings_ranking_central_division(input_json, expected_json):
+    result = standings._ranked_division(["central"], input_json["standings"])
 
-def test_standings_ranking_pacific_division():
-    result = standings._ranked_division(["pacific"], data.teams_2023_12_31)
+    for i, input_team in enumerate(result["Central"]):
+        expected_team_name = expected_json[str(i)]
+        input_team_name = input_team.get('teamName').get('default')
 
-    for i, team in enumerate(result["Pacific"]):
-        assert data.pacific_division_2023_12_31[i] == team.get('teamName').get('default')
+        assert expected_team_name == input_team_name
 
-def test_standings_ranking_eastern_wildcard():
-    result = standings._ranked_wild(["eastern"], data.teams_2023_12_31)
+@pytest.mark.expected_data("division", "pacific")
+def test_standings_ranking_pacific_division(input_json, expected_json):
+    result = standings._ranked_division(["pacific"], input_json["standings"])
 
-    for i, team in enumerate(result["Eastern"]):
-        assert data.eastern_wildcard_2023_12_31[i] == team.get('teamName').get('default')
+    for i, input_team in enumerate(result["Pacific"]):
+        expected_team_name = expected_json[str(i)]
+        input_team_name = input_team.get('teamName').get('default')
 
-def test_standings_ranking_western_wildcard():
-    result = standings._ranked_wild(["western"], data.teams_2023_12_31)
+        assert expected_team_name == input_team_name
 
-    for i, team in enumerate(result["Western"]):
-        assert data.western_wildcard_2023_12_31[i] == team.get('teamName').get('default')
+@pytest.mark.expected_data("wild", "eastern")
+def test_standings_ranking_eastern_wildcard(input_json, expected_json):
+    result = standings._ranked_wild(["eastern"], input_json["standings"])
 
-def test_standings_call_execute_without_parsing(_standings):
+    for i, input_team in enumerate(result["Eastern"]):
+        expected_team_name = expected_json[str(i)]
+        input_team_name = input_team.get('teamName').get('default')
+
+        assert expected_team_name == input_team_name
+
+@pytest.mark.expected_data("wild", "western")
+def test_standings_ranking_western_wildcard(input_json, expected_json):
+    result = standings._ranked_wild(["western"], input_json["standings"])
+
+    for i, input_team in enumerate(result["Western"]):
+        expected_team_name = expected_json[str(i)]
+        input_team_name = input_team.get('teamName').get('default')
+
+        assert expected_team_name == input_team_name
+
+def test_standings_call_execute_without_parsing(standings_object):
     with pytest.raises(SystemExit):
-        _standings.execute()
+        standings_object.execute()
