c400d35e0db1e3c01dcfe4aaad8395c80fe346c3
Alex Reynolds <hellport@protonmail.com>
2024-02-03 14:07:57 -0400

    bump version 0.1.0 -> 0.1.1

--- src/rinkrat/__init__.py
+++ src/rinkrat/__init__.py
@@ -6,7 +6,7 @@ except ModuleNotFoundError:
     import tomli as tomllib
 
 # rinkrat version
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # config exposure
 _cfg = tomllib.loads(resources.read_text("rinkrat", "config.toml"))
